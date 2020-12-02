af701605ca7b8bcb9ed0c7abcdf9e9eeeb832d4d
Jennifer Chen <32661544+Jenayu@users.noreply.github.com>
2020-12-02 14:47:13 -0500

    Update import.R

--- import.R
+++ import.R
@@ -6,10 +6,10 @@ library(ggplot2)
 
 # load data
 # ytable has the average outcome of samples grouped by strains
-qtltable <- fread("C:/Users/Jennifer Chen/Desktop/honors project/data/qtltable.csv", header = TRUE)
+qtltable <- fread("~/data/qtltable.csv", header = TRUE)
 x <- as.matrix(qtltable[,-1])
-ytable <- fread("C:/Users/Jennifer Chen/Desktop/honors project/data/ytable_bw.csv", header = TRUE) 
+ytable <- fread("~/data/ytable_bw.csv", header = TRUE) 
 yBW <- as.matrix(ytable$Dosing.BW)
-ytable <- fread("C:/Users/Jennifer Chen/Desktop/honors project/data/ytable.csv", header = TRUE)
+ytable <- fread("~/data/ytable.csv", header = TRUE)
 yAST <- as.matrix(ytable$AST)
 yALT <- as.matrix(ytable$ALT)
